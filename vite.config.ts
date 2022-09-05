4175e5757566b9f3ed3223bfd5ea7c14e2dab6b5
qing.deng <dengqing0821@gmail.com>
2022-09-05 11:55:26 +0800

    chore:  avoid incorrect asset file names

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,20 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames(asset) {
+          if (asset.name.includes('/src/assets/')) {
+            const filename = asset.name.match(/\/assets\/(.*)/)?.[1]
+            if (filename) {
+              return filename
+            }
+          }
+          return '[name].[hash].[ext]'
+        }
+      }
+    }
+  }
 })
